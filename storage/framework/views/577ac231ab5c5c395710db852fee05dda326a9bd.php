aaaad687ebc7a7d9362bcdbbad1a3bbda79f167b
Sean Christian Lozana <lozanasc@gmail.com>
2022-05-31 08:57:52 +0800

    Callback fixed

---
+++ storage/framework/views/577ac231ab5c5c395710db852fee05dda326a9bd.php
@@ -0,0 +1,57 @@
+
+<?php $__env->startSection('user_root'); ?>
+    <div class="container-fluid">
+    <!-- ============================================================== -->
+    <!-- Start Page Content -->
+    <!-- ============================================================== -->
+    <div class="row">
+        <div class="col-sm-12">
+            <div class="white-box">
+                <h3 class="box-title">List of Books</h3>
+                <div class="table-responsive">
+                    <table class="table text-nowrap">
+                        <thead>
+                            <tr>
+                                <th class="border-top-0">Cover</th>
+                                <th class="border-top-0">ISBN</th>
+                                <th class="border-top-0">Title</th>
+                                <th class="border-top-0">Author</th>
+                                <th class="border-top-0">Physical Qty</th>
+                                <th class="border-top-0">Publication Date</th>
+                                <th class="border-top-0">Actions</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php if(count($books) > 0): ?>
+                                <?php $__currentLoopData = $books; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $book): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+                                    <?php if($book->physical_qty > 0 || $book->digital_copy !== ""): ?>
+                                        <tr>
+                                            <td>
+                                            <img src="<?php echo e(Storage::url($book->book_cover)); ?>" alt="Cover of the book" height="160" width="128">
+                                            </td>
+                                            <td><?php echo e($book->isbn_no); ?></td>
+                                            <td><?php echo e($book->name); ?></td>
+                                            <td><?php echo e($book->author); ?></td>
+                                            <td><?php echo e($book->physical_qty); ?></td>
+                                            <td><?php echo e($book->date_published); ?></td>
+                                            <td>
+                                                <a href="<?php echo e(route('requestBookPreview', $book->id)); ?>" type="button" class="btn btn-primary">Request</a>
+                                                <a href="<?php echo e(route('preview', $book->id)); ?>"  type="button" class="btn btn-warning">Preview</a>
+                                            </td>
+                                        </tr>
+                                    <?php endif; ?>
+                                <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+                            <?php else: ?>
+                            <h3>
+                                No books found!
+                            </h3>
+                            <?php endif; ?>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+        </div>
+    </div>
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('auth0.user.layout.user', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH D:\Repositories\Projects\digital-library\resources\views/auth0/user/books/list.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/d1ff89e0e0748d9354cd104c1fde25d9afccc33d.php
@@ -0,0 +1,89 @@
+
+<?php $__env->startSection('user_root'); ?>
+<div class="container-fluid">
+    <!-- ============================================================== -->
+    <!-- Start Page Content -->
+    <!-- ============================================================== -->
+    <!-- Row -->
+    <div class="row">
+        <!-- Column -->
+        <div class="col-lg-4 col-xlg-3 col-md-12">
+            <div class="white-box">
+                <div class="user-bg"> <img width="100%" alt="user" src="<?php echo e(Storage::url($info->book_cover)); ?>">
+                    <div class="overlay-box">
+                        <div class="user-content">
+                            <a><img src="<?php echo e(Storage::url($info->book_cover)); ?>"
+                            class="thumb-lg img-circle" alt="img"></a>
+                        </div>
+                    </div>
+                </div>
+                <div class="user-btm-box mt-5 d-md-flex">
+                    <strong>Published Date:</strong> <?php echo e($info->date_published); ?>
+
+                </div>
+            </div>
+        </div>
+        <!-- Column -->
+        <div class="col-lg-8 col-xlg-9 col-md-12">
+            <div class="card">
+                <div class="card-body">
+                    <form class="form-horizontal form-material">
+                        <div class="form-group mb-4">
+                            <label class="col-md-12 p-0">ISBN</label>
+                            <div class="col-md-12 border-bottom p-0">
+                                <input disabled type="text"
+                                    value="<?php echo e($info->isbn_no); ?>"
+                                    class="form-control p-0 border-0"> </div>
+                        </div>
+                        <div class="form-group mb-4">
+                            <label for="example-email" class="col-md-12 p-0">Title</label>
+                            <div class="col-md-12 border-bottom p-0">
+                                <input disabled type="text"
+                                    value="<?php echo e($info->name); ?>"
+                                    class="form-control p-0 border-0" name="example-email"
+                                    id="example-email">
+                            </div>
+                        </div>
+                        <div class="form-group mb-4">
+                            <label for="example-email" class="col-md-12 p-0">Author</label>
+                            <div class="col-md-12 border-bottom p-0">
+                                <input disabled type="text"
+                                    value="<?php echo e($info->author); ?>"
+                                    class="form-control p-0 border-0" name="example-email"
+                                    id="example-email">
+                            </div>
+                        </div>
+                        <div class="form-group mb-4">
+                            <label for="example-email" class="col-md-12 p-0">Physical Copies Available</label>
+                            <div class="col-md-12 border-bottom p-0">
+                                <input disabled type="text"
+                                    value="<?php echo e($info->physical_qty); ?>"
+                                    class="form-control p-0 border-0" name="example-email"
+                                    id="example-email">
+                            </div>
+                        </div>
+                        <div class="form-group mb-4">
+                            <label for="example-email" class="col-md-12 p-0">Synopsis</label>
+                            <div class="col-md-12 border-bottom p-0">
+                            <textarea rows="2" disabled class="form-control p-0 border-0"><?php echo e($info->synopsis); ?></textarea>
+                            </div>
+                        </div>
+                        <a href="<?php echo e(route('requestBookPreview', $info->id)); ?>" type="button" class="btn btn-primary">Request</a>
+                        <?php if(Storage::url($info->digital_copy) !== null): ?>
+                        <a 
+                            type="button" class="btn btn-success"
+                            href="<?php echo e(Storage::url($info->digital_copy)); ?>"
+                        >
+                            Download
+                        </a>
+                        <?php endif; ?>
+                    </form>
+                </div>
+            </div>
+        </div>
+        <!-- Column -->
+    </div>
+    <!-- Row -->
+</div>
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('auth0.user.layout.user', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH D:\Repositories\Projects\digital-library\resources\views/auth0/user/books/preview.blade.php ENDPATH**/ ?>
